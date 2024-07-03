8d8c2be312be7c87298cbf21b6dab345cea1e26e
Sourav Das <iamsouravdaswade420@gmail.com>
2024-07-03 11:22:38 +0530

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by sourav das on 03/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
